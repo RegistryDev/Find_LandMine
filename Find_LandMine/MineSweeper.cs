1708464a768a97735cc65c6730d61b55784bd345
Hyeonjun Lee <registry.develop@gmail.com>
2020-12-04 00:43:41 +0900

    btnMineClick btn.Enabled

--- Find_LandMine/MineSweeper.cs
+++ Find_LandMine/MineSweeper.cs
@@ -82,6 +82,7 @@ namespace Find_LandMine
             Button btn = sender as Button;
             btn.Font = new Font("맑은 고딕", 15.0F);
             btn.Text = "💣";
+            btn.Enabled = false;
             btn.BackColor = Color.Gray;
             timer1.Enabled = false;
             MessageBox.Show("실패하셨습니다", "실패");
